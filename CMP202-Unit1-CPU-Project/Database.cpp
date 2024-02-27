e20dde5ce50ac92cd7d3dade5ecd845e4c2a7bfa
Corcso <115189987+Corcso@users.noreply.github.com>
2024-02-27 20:17:14 +0000

    Added: help screen

--- CMP202-Unit1-CPU-Project/Database.cpp
+++ CMP202-Unit1-CPU-Project/Database.cpp
@@ -95,8 +95,8 @@ std::string Database::processCommand(std::string command)
 			// Multi add row follows the following format
 			// MULTIADDROW {table name} {data col 1 row 1} {data col 2 row 1}... {data col 1 row 2} {data col 2 row 2}...
 
-			// Make sure command is correct length (Has to be 2+)
-			if (commandParts[i].size() < 2) return "INVALLID ARGUMENT COUNT";
+			// Make sure command is correct length (Has to be 3+)
+			if (commandParts[i].size() < 3) return "INVALLID ARGUMENT COUNT";
 
 			// Get table via name and then display the first 10 rows, with column headers
 			Table* desiredTable = getDirectTableReference(commandParts[i][1]);
@@ -106,7 +106,7 @@ std::string Database::processCommand(std::string command)
 			std::vector<Table::DataType> colDataTypes = desiredTable->getColTypes();
 
 			// Make sure argument count matches table number of columns + 2(ADDROW and table name) or is exactly 2(adds blank row)
-			if (commandParts[i].size() != 2 && (commandParts[i].size() - 2) % colDataTypes.size() != 0) return "INVALLID ARGUMENT COUNT";
+			if ((commandParts[i].size() - 2) % colDataTypes.size() != 0) return "INVALLID ARGUMENT COUNT";
 
 			// Create a blank row and for every column loop over and add the data
 			// Loop wont run if for blank row
@@ -115,6 +115,25 @@ std::string Database::processCommand(std::string command)
 				for (int col = 0; col < colDataTypes.size(); ++col) {
 					desiredTable->setCellData(Table::convertStringToData(colDataTypes[col], commandParts[i][(row * colDataTypes.size()) + col + 2]), rowIndex, col);
 				}
+			}
+		}
+		else if (commandParts[i][0] == "HELP") {
+			// HELP command give information about other commands and data types
+			// MULTIADDROW {table name} {data col 1 row 1} {data col 2 row 1}... {data col 1 row 2} {data col 2 row 2}...
+
+			if (commandParts[i].size() > 1 && commandParts[i][1] == "TYPES") {
+				std::cout << "Below is a list of available column types: \n"
+					<< "INT_32 | Any number between -2,147,483,648 and 2,147,483,647 inclusive \n"
+					<< "STRING_255 | Any text 255 chars long or less \n";
+			}
+			else {
+				std::cout << "Below is a list of available commands: \n"
+					<< "HELP | This command, displays all commands \n"
+					<< "HELP TYPES | Details all column types \n"
+					<< "Table commands\n"
+					<< "PEEK {table name} | Shows the first 10 rows of a table\n"
+					<< "ADDROW {table name} {data col 1} {data col 2}...  | Adds a row of data\n"
+					<< "MULTIADDROW {table name} {data col 1 row 1} {data col 2 row 1}... {data col 1 row 2} {data col 2 row 2}... | Adds multiple rows of data\n";
 			}
 		}
 		else {
